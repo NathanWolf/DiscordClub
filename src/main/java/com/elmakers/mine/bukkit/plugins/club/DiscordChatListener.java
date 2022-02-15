10e97a7b974df84fa4cf0e57f25ffdf55103270a
Nathan Wolf <nathan@elmakers.com>
2022-02-15 14:05:49 -0800

    Add join role functionality back in

--- src/main/java/com/elmakers/mine/bukkit/plugins/club/DiscordChatListener.java
+++ src/main/java/com/elmakers/mine/bukkit/plugins/club/DiscordChatListener.java
@@ -8,12 +8,18 @@ import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.MessageChannel;
+import net.dv8tion.jda.api.entities.Role;
 import net.dv8tion.jda.api.entities.TextChannel;
 import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.guild.member.GuildMemberJoinEvent;
+import net.dv8tion.jda.api.events.interaction.ButtonClickEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.interactions.components.ActionRow;
+import net.dv8tion.jda.api.interactions.components.Button;
+import net.dv8tion.jda.api.interactions.components.Component;
 import net.dv8tion.jda.api.requests.restaction.MessageAction;
+import net.dv8tion.jda.api.requests.restaction.interactions.ReplyAction;
 
 public class DiscordChatListener extends ListenerAdapter {
     private final ClubController controller;
@@ -25,8 +31,14 @@ public class DiscordChatListener extends ListenerAdapter {
     @Override
     public void onGuildMemberJoin(GuildMemberJoinEvent event) {
         Guild guild = event.getGuild();
+        Role role = getJoinRole(guild);
+        if (role == null) {
+            return;
+        }
 
         Member member = event.getMember();
+        guild.addRoleToMember(member, role).queue(success -> controller.getLogger().info("Assigned join role to " + member.getEffectiveName()), throwable -> controller.getLogger().log(Level.SEVERE, "Failed to assign role to " + member.getEffectiveName(), throwable));
+
         String channelName = controller.getWelcomeChannel();
         List<TextChannel> helpChannels = guild.getTextChannelsByName(channelName, true);
         if (!helpChannels.isEmpty()) {
@@ -34,8 +46,13 @@ public class DiscordChatListener extends ListenerAdapter {
             String welcomeMessage = controller.getMessage("discord.welcome");
             welcomeMessage = welcomeMessage.replace("$member", member.getAsMention());
             MessageAction welcomeAction = helpChannel.sendMessage(welcomeMessage);
+            welcomeAction.setActionRow(getFirstVerifyButton(member));
             welcomeAction.queue(success -> {}, throwable -> controller.getLogger().log(Level.SEVERE, "Failed to send welcome message to " + member.getEffectiveName(), throwable));
         }
+    }
+
+    protected Button getFirstVerifyButton(Member member) {
+        return Button.success("verify:" + member.getId(), controller.getMessage("discord.verify_first_button"));
     }
 
     @Override
@@ -77,5 +94,65 @@ public class DiscordChatListener extends ListenerAdapter {
         String message = controller.respond(originalMessage.getContentStripped());
         MessageAction action = originalMessage.reply(message);
         action.queue(sentMessage -> sentMessage.suppressEmbeds(true).queue(), throwable -> controller.getLogger().log(Level.SEVERE, "Failed to send message in channel " + originalMessage.getChannel(), throwable));
+    }
+
+    private Role getJoinRole(Guild guild) {
+        String joinRole = controller.getJoinRole();
+        if (joinRole.isEmpty()) return null;
+        Role role = guild.getRoleById(joinRole);
+        if (role == null) {
+            controller.getLogger().warning("Invalid join role id: " + joinRole);
+        }
+        return role;
+    }
+
+    @Override
+    public void onButtonClick(ButtonClickEvent event) {
+        MessageChannel channel = event.getChannel();
+        if (channel.getName().equals(controller.getIgnoreChannel())) return;
+
+        Button button = event.getButton();
+        if (button == null) return;
+        String id = button.getId();
+        if (id == null) return;
+        if (id.startsWith("verify:")) {
+            final String memberId = id.substring(7);
+            Member member = event.getMember();
+            if (member == null) return;
+
+            if (!member.getId().equals(memberId)) {
+                ReplyAction action = event.reply(controller.getMessage("discord.verified_invalid"));
+                action.setEphemeral(true);
+                action.queue(sentMessage -> {}, throwable -> controller.getLogger().log(Level.SEVERE, "Failed to send message in response to button click " + id, throwable));
+                return;
+            }
+
+            ReplyAction action = event.reply(controller.getMessage("discord.verified"));
+            action.setEphemeral(true);
+            action.queue(sentMessage -> {}, throwable -> controller.getLogger().log(Level.SEVERE, "Failed to send message in response to button click " + id, throwable));
+
+            // Remove the join button when not on an ephemeral messages
+            Message clickedMessage = event.getMessage();
+            if (!clickedMessage.getFlags().contains(Message.MessageFlag.EPHEMERAL)) {
+                List<ActionRow> actionRows = clickedMessage.getActionRows();
+                if (!actionRows.isEmpty()) {
+                    List<Component> keepButtons = new ArrayList<>();
+                    for (ActionRow row : actionRows) {
+                        for (Component component : row.getComponents()) {
+                            if (!component.equals(button)) {
+                                keepButtons.add(component);
+                            }
+                        }
+                    }
+                    MessageAction editMessage;
+                    if (keepButtons.isEmpty()) {
+                        editMessage = clickedMessage.editMessageComponents();
+                    } else {
+                        editMessage = clickedMessage.editMessageComponents(ActionRow.of(keepButtons));
+                    }
+                    editMessage.queue(sentMessage -> {}, throwable -> controller.getLogger().log(Level.SEVERE, "Failed to remove join button from message " + id, throwable));
+                }
+            }
+        }
     }
 }
