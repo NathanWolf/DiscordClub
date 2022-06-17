e154c10d8adc3c4ad5a6d9526fa240a83286d99d
Nathan Wolf <nathan@elmakers.com>
2022-06-17 16:41:13 -0700

    Goodbye, Eliza- Hello Davinci!

--- src/main/java/com/elmakers/mine/bukkit/plugins/club/ClubController.java
+++ src/main/java/com/elmakers/mine/bukkit/plugins/club/ClubController.java
@@ -1,27 +1,29 @@
 package com.elmakers.mine.bukkit.plugins.club;
 
-import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import org.apache.commons.lang.StringUtils;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.Plugin;
 
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.requests.restaction.MessageAction;
 
-import codeanticode.eliza.Eliza;
-import codeanticode.eliza.FileLoader;
+import com.theokanning.openai.OpenAiService;
+import com.theokanning.openai.completion.CompletionChoice;
+import com.theokanning.openai.completion.CompletionRequest;
 
-public class ClubController implements FileLoader {
+public class ClubController {
     private final Plugin plugin;
     private final YamlConfiguration messages;
-    private final Eliza eliza;
+    private final OpenAiService ai;
     private String token;
     private String channel;
     private String commandChannel;
@@ -39,7 +41,13 @@ public class ClubController implements FileLoader {
 
     public ClubController(Plugin plugin) {
         this.plugin = plugin;
-        eliza = new Eliza(this);
+
+        // Initialize OpenAI
+        String apiKey = plugin.getConfig().getString("api_key", "");
+        if (apiKey.isEmpty()) {
+            throw new IllegalStateException("No api key specified");
+        }
+        ai = new OpenAiService(apiKey);
 
         // Load messages
         messages = new YamlConfiguration();
@@ -78,8 +86,23 @@ public class ClubController implements FileLoader {
         }
     }
 
-    public String respond(String message) {
-        return eliza.processInput(message);
+    public String respond(String prompt) {
+        CompletionRequest completionRequest = CompletionRequest.builder()
+                .temperature(0.9)
+                .prompt(prompt)
+                .echo(false)
+                .maxTokens(150)
+                .topP(1.0)
+                .frequencyPenalty(0.0)
+                .presencePenalty(0.6)
+                .build();
+        List<CompletionChoice> choices = ai.createCompletion("text-davinci-002", completionRequest).getChoices();
+        for (CompletionChoice choice : choices) {
+            if (choice.getFinish_reason().equals("stop")) {
+                return choice.getText().trim();
+            }
+        }
+        return "I don't know what to say.";
     }
 
     public void shutdown() {
@@ -152,25 +175,5 @@ public class ClubController implements FileLoader {
         this.jda = jda;
         jda.getPresence().setActivity(Activity.playing(getMessage("discord.status")));
         getLogger().info("Connected to the Discord server!");
-    }
-
-    @Override
-    public String[] readFile(String filename) {
-        List<String> lines = new ArrayList<>();
-        try {
-            InputStream resource = plugin.getResource(filename);
-            if (resource == null) {
-                throw new Exception(filename + " resource was null");
-            }
-            try (BufferedReader br = new BufferedReader(new InputStreamReader(resource, StandardCharsets.UTF_8))) {
-                String line;
-                while ((line = br.readLine()) != null) {
-                    lines.add(line);
-                }
-            }
-        } catch (Exception ex) {
-            plugin.getLogger().log(Level.SEVERE, "Failed to load messages.yml resource", ex);
-        }
-        return lines.toArray(new String[]{});
     }
 }
